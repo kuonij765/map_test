fa67469d5231146445055d90e97626491b99dbc5
kuonij765 <kuonij765@gmail.com>
2026-05-08 16:15:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ st.code("a=3")
 st.text("안녕하세요 남동고 학생 여러분!!")
 
 # 2. 데이터 읽어보기(데이터 수집 csv)
-df = pd.pd.read_csv('인천광역시 남동구_고등학교_20240325.csv',encoding='cp949')
+df = pd.read_csv('인천광역시 남동구_고등학교_20240325.csv',encoding='cp949')
 df_latlon = df[['위도'],['경도']]
 df_latlon = df_latlon.rename(columns={'위도':'lat','경도':'lon'})
 #st.map(df_latlon)
